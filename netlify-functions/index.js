056c27d412ce118d2cce149e1ce8a09f8996ef18
Bazamus <bazamus@gmail.com>
2025-07-30 19:48:49 +0200

    Fix: Crear directorio netlify-functions específico para funciones serverless

--- netlify-functions/index.js
+++ netlify-functions/index.js
@@ -1,7 +1,7 @@
 // Netlify Function Handler
 // Este archivo sirve como punto de entrada para las funciones serverless de Netlify
 
-const { handler } = require('./dist/index.js');
+const { handler } = require('../backend/dist/index.js');
 
 // Exportar el handler para Netlify Functions
 module.exports = handler; 
